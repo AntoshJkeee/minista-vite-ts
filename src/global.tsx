62b42c5fc204bee3d825fb5c1846028717e089c7
AntoshJkeee <toha_bart@mail.ru>
2025-03-03 20:56:50 +0300

    add prettier

--- src/global.tsx
+++ src/global.tsx
@@ -1,41 +1,43 @@
-import "@/styles";
-import { Head, PageProps } from "minista"
-import { Header, Content, Footer } from "@/layouts"
-import { Banner } from "@/sections";
+import '@/styles'
+import { Head, PageProps } from 'minista'
+import { Header, Content, Footer } from '@/layouts'
+import { Banner } from '@/sections'
 
 export default (props: PageProps) => {
-	const { children, title, url, isHeaderFixed, withCss, listPages} = props
+	const { children, title, url, isHeaderFixed, withCss, listPages } = props
 
 	return (
 		<>
-			<Head htmlAttributes={{
-				lang: 'en'
-			}}>
+			<Head
+				htmlAttributes={{
+					lang: 'en',
+				}}
+			>
 				<title>Stream Vibe | {title}</title>
-				<script src="/src/main.ts" type='module'/>
-				<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
-				<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
-				<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
-				<link rel="manifest" href="/site.webmanifest"/>
+				<script src="/src/main.ts" type="module" />
+				<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+				<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+				<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+				<link rel="manifest" href="/site.webmanifest" />
 				{withCss && <link rel="stylesheet" href="/src/assets/css/entry.scss" />}
 			</Head>
 
 			{!listPages ? (
-					<>
-						<Header url={url} isFixed={isHeaderFixed}/>
-						<Content isResetPaddingTop={isHeaderFixed}>
-							{children}
-							<div data-js-fake-api="/fake-api/test.json">
-								<button>Click</button>
-							</div>
-							<Banner/>
-							<div className="entry">Entry css</div>
-						</Content>
-						<Footer/>
-					</>
-				)
-				:
-				<>{children}</>}
+				<>
+					<Header url={url} isFixed={isHeaderFixed} />
+					<Content isResetPaddingTop={isHeaderFixed}>
+						{children}
+						<div data-js-fake-api="/fake-api/test.json">
+							<button>Click</button>
+						</div>
+						<Banner />
+						<div className="entry">Entry css</div>
+					</Content>
+					<Footer />
+				</>
+			) : (
+				<>{children}</>
+			)}
 		</>
 	)
 }
